1ad41ff96fcfe69eb0346b1dcc4fd53cddb108b4
Clement Tong <clement.yc.tong@gmail.com>
2022-08-10 18:54:13 +0800

    Getting Chip Data

--- main/blink.c
+++ main/blink.c
@@ -6,6 +6,9 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include <string.h>
+#include "esp_spi_flash.h"
+#include "esp_flash.h"
+#include "esp_system.h"
 
 #define TAG "KEYBOARD INPUT"
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
@@ -14,19 +17,33 @@
 
 void app_main(void)
 {
-    char c = 0;
-    char str[100];
-    memset(str,0,sizeof(str));
-    while(c!='\n')
-    {
-        c = getchar();
-        if (c != 0xff) //if no character is pressed, it will be 0xff , this is a non blocking function
-        {
-            str[strlen(str)] = c;
-            printf("%c",c);
-        }
-        vTaskDelay(100/portTICK_RATE_MS);
-    }
+    /*printf("Hi 1234");
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("hello world");
+    printf("number of cores %d",chip_info.cores);*/
+    printf("Hello world!\n");
 
-    printf("you typed %s\n",str);
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    fflush(stdout); // this seems to be key to make things print
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    //fflush(stdout); // this seems to be key to make things print
+    //printf("silicon revision %d, ", chip_info.revision);
+
+    //printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    /*for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();*/
+
 }
